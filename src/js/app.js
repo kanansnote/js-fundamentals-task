900ba7bbcb5323cb568b6b3a7a33705e99fb05b3
kanan <kanansnote@gmail.com>
2024-05-16 19:54:06 +0400

    Added solved tasks from 4 to 7.

--- src/js/app.js
+++ src/js/app.js
@@ -73,3 +73,98 @@
 // }
 
 // findUniqueElements('html, css, js, html, bootstrap, js, react');
+
+// Task 4
+
+// function camelize(text) {
+
+    // My Code
+    // let strArray = str.split('-');
+    // // console.log(strArray);
+    // let arrayCamelize = strArray[strArray.length - 1];
+    // let firstLetterUppercased = arrayCamelize.slice(0, 1).toUpperCase();
+    // let otherRemainingsFromSlice = arrayCamelize.slice(1);
+    // let remainingString = strArray[0] + firstLetterUppercased + otherRemainingsFromSlice;
+    // let firstDashedWord = ;
+    // console.log(remainingString);
+
+    // Mentor's Code
+
+//     const arr = text.split('-');
+//     const newArr = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i == 0) {
+//             newArr.push(arr[i]);
+//         } else {
+//             //newArr.push(arr[i][0].toUpperCase() + arr[i].slice(1));
+//             newArr.push(arr[i].replace(arr[i][0], arr[i][0].toUpperCase()))
+//         }
+//     }
+
+//     console.log(newArr.join(""));
+// }
+
+// camelize('background-color') == 'backgroundColor';
+// camelize('list-style-image') == 'listStyleImage';
+// camelize('-webkit-transition') == 'WebkitTransition';
+
+// Task 5
+
+// function minMax(arr) {
+//     let min = arr[0];  //2
+//     let max = arr[0];  // 445
+
+//     for (let i = 0; i<arr.length; i++) {
+//         if (arr[i] > max) {
+//             max = arr[i];  
+//         }
+
+//         if (arr[i] < min) {
+//             min = arr[i]; // 445 > 3
+//         }
+//     }
+
+//     console.log(`Min: ${min}, max: ${max}`);
+// }
+
+// minMax([2, 3, 445, 6, 1]); //min 2, max 445
+
+// Task 6
+
+// function oddEvenNumbers(arr){
+
+//     let newArr = [];
+    
+//     for (let i = 0; i < arr.length; i++) {
+//         if (!(i % 2 == 0)) {
+//             newArr.push(arr[i]*2);
+//         } else {
+//             newArr.push(arr[i]);
+//         }
+//     }
+
+//     console.log(newArr);
+// }
+
+// oddEvenNumbers([1,2,3,4]);
+
+// Task 7
+
+// function removeSmallest(arr) {
+//     let smallNumber = arr[0];
+//     let newArr = [];
+    
+//     for (let i = 0; i < arr.length; i++){
+
+//         if (smallNumber < arr[i]) {
+//             newArr.push(arr.slice(1))
+//         }
+//     }
+
+//     console.log(newArr);
+// }
+
+// removeSmallest([1,2,3,4,5]);
+// removeSmallest([5,4,3,2,1]);
+// removeSmallest([2,2,1,2,1]);
