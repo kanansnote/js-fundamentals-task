bcc87829200bd5ff570afa7cb68f2d9bfa075a9b
kanan <kanansnote@gmail.com>
2024-05-27 18:39:57 +0400

    Added unsolved task related to Objects.

--- src/js/app.js
+++ src/js/app.js
@@ -117,7 +117,7 @@
 
 //     for (let i = 0; i<arr.length; i++) {
 //         if (arr[i] > max) {
-//             max = arr[i];  
+//             max = arr[i];
 //         }
 
 //         if (arr[i] < min) {
@@ -419,3 +419,77 @@
 // console.log(isAvgWhole([1, 5, 6])); // true
 // console.log(isAvgWhole([1, 1, 1])); // true
 // console.log(isAvgWhole([9, 2, 2, 5])); // false
+
+// Task 24
+// Objects
+
+const game = {
+	team1: "Bayern Munich",
+	team2: "Borrussia Dortmund",
+	players: [
+		[
+			"Neuer",
+			"Pavard",
+			"Martinez",
+			"Alaba",
+			"Davies",
+			"Kimmich",
+			"Goretzka",
+			"Coman",
+			"Muller",
+			"Gnarby",
+			"Lewandowski",
+		],
+		[
+			"Burki",
+			"Schulz",
+			"Hummels",
+			"Akanji",
+			"Hakimi",
+			"Weigl",
+			"Witsel",
+			"Hazard",
+			"Brandt",
+			"Sancho",
+			"Gotze",
+		],
+	],
+	score: "4:0",
+	scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+	date: "Nov 9th, 2037",
+	odds: {
+		team1: 1.33,
+		x: 3.25,
+		team2: 6.5,
+	},
+};
+// Task-1
+const [players1, players2] = game.players;
+
+// Task-2
+const [gk, ...fieldPlayers] = players1;
+// console.log(gk);
+// console.log(fieldPlayers);
+
+// Task-3
+
+const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+  
+// Task-4
+const players1Final = [...players1,'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// Task-5
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+
+// Task-6
+function printGoals(...arr) {
+    console.log(arr);
+}
+
+printGoals('Witsel', 'Hazard');
+
+// Task-7
+console.log(team1 > team2);
